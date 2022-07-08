661affb92942ec2ad7276d347182227568b196e4
San <odyenri@gmail.com>
2022-07-08 18:53:52 -0400

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,8 @@ import Tori from './components/Tori.vue'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
